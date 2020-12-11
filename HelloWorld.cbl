33a65709953e5559834554bd873e97d1a19f13bd
Aaron Lee <aaronlab.net@gmail.com>
2020-12-11 20:46:52 +0900

    Hello World

---
+++ HelloWorld.cbl
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author: Aaron Lee
+      * Date: 2020-12-11
+      * Purpose: I AM BORED
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. YOUR-PROGRAM-NAME.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world"
+            STOP RUN.
+       END PROGRAM YOUR-PROGRAM-NAME.
